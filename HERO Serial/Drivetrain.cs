19f984e95094a1bbf4826c2c99d67ec526e5263a
DarkenedDragon <eridor314@gmail.com>
2021-11-10 18:46:14 -0500

    Created Drivetrain singleton

---
+++ HERO Serial/Drivetrain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CTRE.Phoenix.MotorControl.CAN;
+using CTRE.Phoenix.MotorControl;
+using CTRE.Phoenix.Sensors;
+
+
+public class Drivetrain
+
+	private static readonly Drivetrain instance;
+	private static readonly TalonSRX leftLeader;
+{
+	private Drivetrain()
+	{
+		// This is a singleton
+		// All the talons for the drivetrain live here
+		// Have an init, then several drive functions
+		// All the talons on one side will follow a leader talon
+
+		leftLeader = new TalonSRX();
+		leftFollower = new TalonSRX();
+		fdsklje
+
+
+	}
+
+	public Drivetrain getInstance()
+    {
+		if this.instance == null{
+			this.instance = new Drivetrain();
+        }
+
+		return this.instance;
+    }
+}
---
+++ HERO Serial/HERO Serial/Drivetrain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CTRE.Phoenix.MotorControl.CAN;
+using CTRE.Phoenix.MotorControl;
+using CTRE.Phoenix.Sensors;
+
+
+public class Drivetrain
+{ 
+	private static Drivetrain instance;
+	private readonly TalonSRX leftLeader;
+	private readonly TalonSRX leftFollower;
+	private readonly TalonSRX rightLeader;
+	private readonly TalonSRX rightFollower;
+
+	private Drivetrain()
+	{
+		// This is a singleton
+		// All the talons for the drivetrain live here
+		// Have an init, then several drive functions
+		// All the talons on one side will follow a leader talon
+
+		leftLeader = new TalonSRX(4);
+		leftFollower = new TalonSRX(5);
+		rightLeader = new TalonSRX(6);
+		rightFollower = new TalonSRX(7);
+
+
+
+	}
+
+	public static Drivetrain getInstance()
+    {
+		if (instance == null){
+			instance = new Drivetrain();
+        }
+
+		return instance;
+    }
+}
